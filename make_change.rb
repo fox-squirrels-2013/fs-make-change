7f08e6abf3e60e25290e0ce6a28e23fc0d3757a9
Myles Byrne <myles@myles.id.au>
2013-10-29 15:41:08 -0700

    Cleanup

--- make_change.rb
+++ make_change.rb
@@ -1,9 +1,8 @@
-# you know when you go to order a sandwich in America
-# and you give them money and they give you change
-# there's a robot that makes the change for you
-def assert(test)
-	raise "Boom" unless test
-end
+# The final goal:
+#
+# 		make_change(110) #=> {:dollar => 1, :dime => 1}
+
+# The implementation
 
 def make_change(cents)
   coins = {}
@@ -18,9 +17,13 @@ def make_change(cents)
   coins
 end
 
+# The test support/harness/framework
 
+def assert(test)
+	raise "Boom" unless test
+end
 
-# make_change(110) #=> {:dollar => 1, :dime => 1}
+# The tests
 
 assert(make_change(0) == {})
 assert(make_change(1) == {:pennies => 1})
@@ -29,47 +32,4 @@ assert(make_change(5) == {:nickels => 1})
 assert(make_change(6) == {:nickels => 1, :pennies => 1})
 # assert(make_change(7) == {:nickels => 1, :pennies => 2})
 
-puts "\'Oh Hey\'"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# design decisions:
-#
-# 1. naming the function
-# ---------------------
-# change_machine
-# return_change
-# make_change    					<-- let's go with this
-# calculate_change
-#
-# 2. shape of the input
-# ---------------------
-# floating point
-# integer 								<-- let's go with this
-#
-# 3. shape of the output
-# ----------------------
-# map (ruby calls this a 'hash')  <-- let's go with this
-# array?
-#
-# 4. naming the input
-# ----------------------
-# change
-# amount
-# less_than_dollar
-# cents  									<-- let's go with this
-#
-# 5. name the output
-# ----------------------
-# coins
+puts "OK"
