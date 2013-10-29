e5ab9278554a6497532b8f8d8e184e0ae28dd2c7
Sherif Abushadi <sherif@amgando.org>
2013-10-29 14:41:43 -0700

    make critical design decisions

--- play.rb
+++ play.rb
@@ -1,7 +1,38 @@
 # you know when you go to order a sandwich in America
 # and you give them money and they give you change
 # there's a robot that makes the change for you
-def ___(___)
+def make_change(cents)
 
-	return ___
+
+	return coins
 end
+
+# design decisions:
+#
+# 1. naming the function
+# ---------------------
+# change_machine
+# return_change
+# make_change    					<-- let's go with this
+# calculate_change
+#
+# 2. shape of the input
+# ---------------------
+# floating point
+# integer 								<-- let's go with this
+#
+# 3. shape of the output
+# ----------------------
+# map (ruby calls this a 'hash')  <-- let's go with this
+# array?
+#
+# 4. naming the input
+# ----------------------
+# change
+# amount
+# less_than_dollar
+# cents  									<-- let's go with this
+#
+# 5. name the output
+# ----------------------
+# coins
