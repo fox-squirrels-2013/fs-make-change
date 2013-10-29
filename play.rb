680c0f6e567331d50b1dec3c89c354c916e520ef
Sherif Abushadi <sherif@amgando.org>
2013-10-29 14:27:53 -0700

    choose the design, define the problem

---
+++ play.rb
@@ -0,0 +1,7 @@
+# you know when you go to order a sandwich in America
+# and you give them money and they give you change
+# there's a robot that makes the change for you
+def ___(___)
+
+	return ___
+end
